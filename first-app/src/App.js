24713fe397353866e0948e71b2a6d2d7aee1574c
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-08-01 19:03:29 +0530

    learnt about creating react app and vite app

---
+++ first-app/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello from day1</h1>
+  );
+}
+
+export default App;
---
+++ first-vite-app/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+  return (
+    <h1>REACT DAY 1 WITH VITE</h1>
+  
+  )
+}
+
+export default App
