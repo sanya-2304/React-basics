a23f6b9c587125db9b8a73651873f58cdb57f3b4
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-08-02 16:16:02 +0530

    custom react comp in vite

--- first-vite-app/src/main.jsx
+++ first-vite-app/src/main.jsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Hi from './Hi.jsx'
 
+const CustomEl=React.createElement(
+  'a', {
+    href:"https://netflix.com", target:"_blank"
+  }, 'click here to visit Netflix'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
     <Hi/>
+    {CustomEl}
   </React.StrictMode>,
 )
