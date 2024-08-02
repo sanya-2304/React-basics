d520fdda07d7dc0469ce0ecc3b0e7082967d134c
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-08-02 18:39:18 +0530

    counter App

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  let [counter, setCounter]=useState(15);
+  // let counter=50;
+  const addVal=()=>{
+    setCounter(counter+1)
+    console.log('clicked ',{counter})
+  }
+  const remVal=()=>{
+    setCounter(counter-1)
+    console.log('clicked ',{counter})
+  }
+
+  return (
+    <>
+    <h1>Counter App</h1>
+    
+    <button onClick={addVal}>AddVal {counter} </button> <h2>Counter val: {counter}</h2>
+    <button onClick={remVal}>remVal: {counter} </button>
+    </>
+  )
+}
+
+export default App
